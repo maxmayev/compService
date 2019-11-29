9cd2de6413672491fadb2e0d1ce613ba07370731
maxmayev <maxmayev@gmail.com>
2019-11-29 21:07:05 +0300

    fix technic and order fields

--- src/main/java/com/maxmayev/compservice/DAO/OrderRepositoryImpl.java
+++ src/main/java/com/maxmayev/compservice/DAO/OrderRepositoryImpl.java
@@ -27,9 +27,13 @@ public class OrderRepositoryImpl implements OrderRepository {
     }
 
     private Order mapRowToOrder(ResultSet rs, int rowNum) throws SQLException{
+        Order.Technic technic = rs.getString("technic") == null ? Order.Technic.NotSet : Order.Technic.valueOf(rs.getString("technic"));
+        Order.ConditionType conditionType =  rs.getString("conditionType") == null ? Order.ConditionType.NotSet : Order.ConditionType.valueOf(rs.getString("conditionType"));
+        log.info("Tecnick " + rs.getString("technic"));
+        log.info("Condition " + rs.getString("conditionType"));
         Order order = new Order(rs.getInt("id"), rs.getDate("appenddate"), rs.getDate("receiveplan"), rs.getDate("receivefact"), rs.getDate("calldate"),
-                rs.getInt("active"),Order.Technic.Other /*Order.Technic.valueOf(rs.getString("technic"))*/,rs.getString("brand"), rs.getString("model"), rs.getString("serialnumber"),
-                /*Order.ConditionType.valueOf(rs.getString("conditionType"))*/Order.ConditionType.NotSet, rs.getString("conditiondescription"), rs.getString("orderdescription"), rs.getString("note"));
+                rs.getInt("active"),technic,rs.getString("brand"), rs.getString("model"), rs.getString("serialnumber"),
+                conditionType, rs.getString("conditiondescription"), rs.getString("orderdescription"), rs.getString("note"));
         log.info(order.toString());
         return order;
     }
